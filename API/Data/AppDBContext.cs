4a176ea6705e21c8239f010d505727a7bd095722
wivelsted <vika.forever.21@gmail.com>
2025-08-11 12:38:17 +0200

    Added Booking model

--- API/Data/AppDBContext.cs
+++ API/Data/AppDBContext.cs
@@ -10,6 +10,7 @@ namespace API.Data
         {
         }
         public DbSet<User> Users { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
     }
 }
 
---
+++ DomainModels/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModels;
+public class Booking
+{
+	public required int Id { get; set; }
+	//public Room RoomBooked { get; set; }
+	public required DateTime CheckIn { get; set; }
+	public required DateTime CheckOut { get; set; }
+	public int NightsCount { get; set; } 
+	public int GuestsCount { get; set; } 
+	public string? Payment {  get; set; }
+	public bool IsPaid { get; set; }
+	public required int UserId { get; set; }
+
+	[ForeignKey("UserId")]
+	public User User { get; set; }
+
+}
