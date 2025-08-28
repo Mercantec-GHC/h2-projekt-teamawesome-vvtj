eef2578cb4d1eebcc273bda1e51a83c683b19dd1
wivelsted <vika.forever.21@gmail.com>
2025-08-28 10:59:28 +0200

    Login page

--- Blazor/Pages/Auth/Login.razor.cs
+++ Blazor/Pages/Auth/Login.razor.cs
@@ -1,6 +1,31 @@
-﻿namespace Blazor.Pages
+﻿using Blazor.Interfaces;
+using Blazor.Models.Dto.Auth;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Pages.Auth;
+
+public partial class Login
 {
-	public class Login
+	[Inject] 
+	private IAuthService _authService { get; set; } = null!;
+	[Inject] 
+	private NavigationManager _navigation{ get; set; } = null!;
+	private string _errorMessage = string.Empty;
+	private LoginDto _loginModel = new();
+
+
+	private async Task HandleLogin()
 	{
+		_errorMessage = string.Empty;
+	
+		var result = await _authService.LoginAsync(_loginModel.Email, _loginModel.Password);
+		if (result)
+		{
+			//_navigation.NavigateTo("/");
+		}
+		else
+		{
+			_errorMessage = "Invalid email or password.";
+		}
 	}
 }
