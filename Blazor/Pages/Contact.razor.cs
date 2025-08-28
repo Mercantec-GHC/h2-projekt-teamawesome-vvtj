69819989da9dd802641b21635cecfc59f245b337
vlbu0001 <113466734+bukh04@users.noreply.github.com>
2025-08-28 12:35:35 +0200

    contact in process

---
+++ Blazor/Pages/Contact.razor.cs
@@ -0,0 +1,14 @@
+﻿using DomainModels.Dto;
+
+namespace Blazor.Pages
+{
+    public partial class Contact
+    {
+        public List<HotelDto> Hotels { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Hotels = (await ApiService.GetAllHotelsAsync())?.ToList() ?? new List<HotelDto>();
+        }
+    }
+}
