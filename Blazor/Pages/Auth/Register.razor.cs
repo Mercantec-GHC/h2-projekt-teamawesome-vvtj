a42b71fbcd87c97e23ba875d426e790239eaf6fd
wivelsted <vika.forever.21@gmail.com>
2025-08-31 10:14:04 +0200

    Register form

--- Blazor/Pages/Auth/Register.razor.cs
+++ Blazor/Pages/Auth/Register.razor.cs
@@ -1,5 +1,45 @@
-﻿namespace Blazor.Pages.Auth;
+﻿using Blazor.Interfaces;
+using Blazor.Models.Dto.Auth;
+using Microsoft.AspNetCore.Components;
 
-public partial class Reagister
+namespace Blazor.Pages.Auth;
+
+public partial class Register
 {
+	[Inject]
+	private IAuthService _authService { get; set; } = null!;
+	[Inject]
+	private NavigationManager _navigation { get; set; } = null!;
+	private string _errorMessage = string.Empty;
+	private string _successMessage = string.Empty;
+	private RegisterDto _registerModel = new();
+
+	private async Task HandleRegister()
+	{
+		_errorMessage = string.Empty;
+		_successMessage = string.Empty;
+
+		if (_registerModel.Password != _registerModel.ConfirmPassword)
+		{
+			_errorMessage = "Passwords do not match.";
+			return;
+		}
+
+		var result = await _authService.RegisterAsync(
+		_registerModel.Email,
+		_registerModel.Username,
+		_registerModel.Password,
+		_registerModel.ConfirmPassword
+		);
+
+		if (result)
+		{
+			_successMessage = "Registration successful! You can now log in.";
+			_navigation.NavigateTo("/login");
+		}
+		else
+		{
+			_errorMessage = "Registration failed. Please try again.";
+		}
+	}
 }
