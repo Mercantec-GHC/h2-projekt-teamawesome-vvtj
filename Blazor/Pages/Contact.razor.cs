30ea4b1fbd885d401b6f42b9b994ed55c1b8c23c
vlbu0001 <113466734+bukh04@users.noreply.github.com>
2025-09-01 14:59:54 +0200

    added email form, email controller, fake smtp service, contact page

--- Blazor/Pages/Contact.razor.cs
+++ Blazor/Pages/Contact.razor.cs
@@ -1,14 +1,51 @@
-﻿using DomainModels.Dto;
+﻿using Blazor.Models.ViewModel;
+using DomainModels.Dto;
+using Microsoft.AspNetCore.Components;
 
 namespace Blazor.Pages
 {
     public partial class Contact
     {
         public List<HotelDto> Hotels { get; set; } = new();
+        [SupplyParameterFromForm(FormName = "ContactForm")]
+        ContactViewModel contactForm { get; set; } = new();
 
+        bool isSent = false;
+        bool isError = false;
         protected override async Task OnInitializedAsync()
         {
             Hotels = (await ApiService.GetAllHotelsAsync())?.ToList() ?? new List<HotelDto>();
+        }
+
+        private async Task HandleSubmit()
+        {
+            isSent = false;
+            isError = false;
+
+            try
+            {
+                var contactDto = new EmailFormDto
+                {
+                    Name = contactForm.Name,
+                    Email = contactForm.Email,
+                    Message = contactForm.Message
+                };
+
+                var request = await ApiService.SendEmailAsync(contactDto);
+
+                if (request)
+                {
+                    isSent = true;
+                }
+                else
+                {
+                    isError = true;
+                }
+            }
+            catch (Exception)
+            {
+                isError = true;
+            }
         }
     }
 }
