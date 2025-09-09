a834e67604d4cb8cd1275a275a7838418d8bdc39
wivelsted <vika.forever.21@gmail.com>
2025-09-09 09:27:24 +0200

    Services + page + styling

---
+++ Blazor/Pages/User/UserProfile.razor.cs
@@ -0,0 +1,31 @@
+﻿using Blazor.Models.ViewModels;
+using Blazor.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Pages.User;
+
+public partial class UserProfile : ComponentBase
+{
+	[Inject]
+	private APIService _apiService { get; set; } = default!;
+	[Inject]
+	private CustomAuthStateProvider _authState { get; set; } = default!;
+	[Parameter]
+	public string? UserId { get; set; }
+	private CurrentUserProfileViewModel? _userProfileVM { get; set; }
+	protected override async Task OnInitializedAsync()
+	{
+		var authState = await _authState.GetAuthenticationStateAsync();
+
+		if (authState.User.Identity?.IsAuthenticated ?? false)
+		{
+			var user = await _apiService.GetCurrentUserInfoAsync();
+
+			if (user != null)
+			{
+				_userProfileVM = user.ToViewModel();
+			}
+		}
+		
+	}
+}
