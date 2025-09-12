9c747d7dd3d8e9c2137368626db5cc58dc4bd793
Vladyslav Bukhanevych <vbu@ewe.dk>
2025-09-12 12:16:27 +0200

    update service-worker

--- Blazor/wwwroot/service-worker.js
+++ Blazor/wwwroot/service-worker.js
@@ -15,3 +15,13 @@ self.addEventListener('push', event => {
         })
     );
 });
+
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    clients.claim();
+});
+
