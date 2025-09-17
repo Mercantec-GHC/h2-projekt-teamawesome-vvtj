5b9e811720dff278318c122ea3d8f6f52ff448f4
Vladyslav Bukhanevych <vbu@ewe.dk>
2025-09-17 13:54:36 +0200

    .

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,25 @@
+﻿import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './feature/auth/Login'
+import { Index } from './feature/apps/Index'
+import { ProtectedRoute } from './feature/auth/ProtectedRoute'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Index />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
