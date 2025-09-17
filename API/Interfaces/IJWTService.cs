9fa856f2b551201a6af374c60c06f262895002ec
wivelsted <vika.forever.21@gmail.com>
2025-09-17 16:09:42 +0200

    Adding dedicated JWTService; Refactoring AuthService and ActiveDirectoryService + controllers.

---
+++ API/Interfaces/IJWTService.cs
@@ -0,0 +1,24 @@
+﻿using DomainModels.Models;
+using static API.Services.ActiveDirectoryService;
+
+namespace API.Interfaces;
+
+/// <summary>
+/// Service for creating JWT tokens for application and AD users.
+/// </summary>
+public interface IJWTService
+{
+	/// <summary>
+	/// Creates a JWT token for a standard application user.
+	/// </summary>
+	/// <param name="user">The application user.</param>
+	/// <returns>A JWT token string.</returns>
+	string CreateToken(User user);
+
+	/// <summary>
+	/// Creates a JWT token for an Active Directory user.
+	/// </summary>
+	/// <param name="adUser">The AD user information.</param>
+	/// <returns>A JWT token string.</returns>
+	string CreateToken(ADUserInfo adUser);
+}
