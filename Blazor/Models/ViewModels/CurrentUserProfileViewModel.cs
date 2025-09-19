e46508de48dae8872bdbc6e215fd70a8bb55d1d5
wivelsted <vika.forever.21@gmail.com>
2025-09-19 11:01:39 +0200

    creating new pages EditProfile + VM + extentions

--- Blazor/Models/ViewModels/CurrentUserProfileViewModel.cs
+++ Blazor/Models/ViewModels/CurrentUserProfileViewModel.cs
@@ -2,15 +2,17 @@
 
 public class CurrentUserProfileViewModel
 {
-	public string? FirstName { get; set; } 
-	public string? LastName { get; set; }
-	public string? Address { get; set; }
+	public string FirstName { get; set; } = "";
+	public string LastName { get; set; } = "";
+	public string Address { get; set; } = "";
 	public string? PostalCode { get; set; }
-	public string? Country { get; set; }
 	public string? City { get; set; }
-	public string? PhoneNumber { get; set; }
+	public string? Country { get; set; }
+	public string PhoneNumber { get; set; } = "";
 	public DateOnly? DateOfBirth { get; set; }
 	public string? SpecialRequests { get; set; }
-	public DateTime? UpdatedAt { get; set; }
-	public string? LastUpdated { get; set; }
+	public string? CreatedAtFormated { get; set; }
+	public DateTime? CreatedAt { get; set; }
+	public string? LastUpdatedFormated { get; set; }
+	public DateTime? LastUpdated { get; set; }
 }
---
+++ Blazor/Pages/User/EditUserProfile.razor.cs
@@ -0,0 +1,57 @@
+﻿using Blazor.Models.ViewModels;
+using Blazor.Services;
+using BlazorBootstrap;
+using DomainModels.Dto.UserProfileDto;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Pages.User;
+
+public partial class EditUserProfile : ComponentBase
+{
+	[Inject]
+	private APIService _apiService { get; set; } = default!;
+	[Inject]
+	private CustomAuthStateProvider _authState { get; set; } = default!;
+	[Inject]
+	protected PreloadService PreloadService { get; set; } = default!;
+	private CurrentUserProfileViewModel? _userProfileVM { get; set; } = new();
+	[SupplyParameterFromForm(FormName = "EditInfoForm")]
+	CurrentUserProfileViewModel EditInfoForm { get; set; } = new();
+
+	protected override async Task OnInitializedAsync()
+	{
+		PreloadService.Show();
+		var currentUser = await _apiService.GetCurrentUserInfoAsync();
+		PreloadService.Hide();
+		if (currentUser != null)
+		{
+			_userProfileVM = currentUser.ToViewModel();
+			EditInfoForm = currentUser.ToViewModel();
+		}
+	}
+
+	private async Task HandleOnValidSubmit()
+	{
+		if (EditInfoForm != null)
+		{
+			var userInfoPutDto = new UserInfoPutDto();
+			EditInfoForm.ToUserInfoPutDto(EditInfoForm, userInfoPutDto);
+
+			PreloadService.Show();
+			var success = await _apiService.UpdateUserInfoAsync(userInfoPutDto);
+
+			PreloadService.Hide();
+
+			StateHasChanged();
+			if (success)
+			{
+				var updatedUser = await _apiService.GetCurrentUserInfoAsync();
+				if (updatedUser != null)
+				{
+					EditInfoForm = updatedUser.ToViewModel();
+				}
+			}
+
+		}
+	}
+}
