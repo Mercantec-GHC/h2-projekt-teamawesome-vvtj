804255d6c352ae9fb98a500346f1423bc30c5685
Vladyslav Bukhanevych <vbu@ewe.dk>
2025-09-22 11:49:34 +0200

    added room types  and edit, filter by hotels in rooms

--- dashboard/src/components/app-sidebar.tsx
+++ dashboard/src/components/app-sidebar.tsx
@@ -93,7 +93,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           },
           {
             title: "Room types",
-            url: "#",
+            url: "/room-types",
           },
         ],
       },
