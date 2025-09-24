8730cc196a05bf09b363202d0c27b6698e0007c5
bukh04 <buhanevych04@ukr.net>
2025-09-24 11:54:18 +0200

    in process

--- API/Services/EmailService.cs
+++ API/Services/EmailService.cs
@@ -46,5 +46,39 @@ namespace API.Services
                 return false;
             }
         }
-    }
+
+		public async Task<bool> SendWelcomeEmailAsync(EmailFormDto dto)
+		{
+			try
+			{
+				var email = new MimeMessage();
+
+				email.From.Add(new MailboxAddress("NOVA Hotels Team", "noreply@novahotels.com"));
+				email.To.Add(MailboxAddress.Parse(dto.Email));
+				email.Subject = "Welcome to NOVA Hotels!";
+
+				email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+				{
+					Text = $@"
+                    <h1>Congratulations, {dto.Name}!</h1>
+                    <p>Thank you for registering with NOVA Hotels. Your account has been successfully created.</p>
+                    <p>You can now log in and start booking rooms.</p>
+                    <a href='[YOUR LOGIN LINK]'>Log in</a>
+                    "};
+
+				using var smtp = new SmtpClient();
+				await smtp.ConnectAsync("sandbox.smtp.mailtrap.io", 587, SecureSocketOptions.StartTls);
+				await smtp.AuthenticateAsync("a7342b2a3cc877", "5f0cabeff0b224"); 
+				await smtp.SendAsync(email);
+				await smtp.DisconnectAsync(true);
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"[Welcome Email Error for {dto.Email}]: {ex.Message}");
+				return false;
+			}
+		}
+	}
 }
