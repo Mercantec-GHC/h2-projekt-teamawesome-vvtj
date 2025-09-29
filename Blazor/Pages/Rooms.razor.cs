aa28b1eb5fe3a36312278fe2c862b113fd04338f
Jasmin May Anderson <05jasmay@gmail.com>
2025-09-29 13:36:47 +0200

    Simplify room image and title rendering in Rooms page

--- Blazor/Pages/Rooms.razor.cs
+++ Blazor/Pages/Rooms.razor.cs
@@ -20,7 +20,7 @@ namespace Blazor.Pages
         {
             System.Console.WriteLine("OnInitializedAsync: Getting room types");
             var result = await _aPIService.GetRoomTypeAsync()
-            ?? throw new ArgumentException("Error: No Room types were found");
+                ?? throw new ArgumentException("Error: No Room types were found");
             roomTypes = result?.Where(r => r != null)!;
         }
     }
