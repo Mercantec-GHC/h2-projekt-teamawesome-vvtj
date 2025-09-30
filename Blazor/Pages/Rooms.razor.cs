55275ea6866d04728b75b5e2cea3554f6e168700
Jasmin May Anderson <05jasmay@gmail.com>
2025-09-30 13:45:14 +0200

    Add preload indicator to Rooms page

--- Blazor/Pages/Rooms.razor.cs
+++ Blazor/Pages/Rooms.razor.cs
@@ -4,24 +4,31 @@ using DomainModels.Dto;
 using Microsoft.AspNetCore.Components;
 using DomainModels.Models;
 using Blazor.Services;
-using System.Threading.Tasks;
-using System.Linq;
+using BlazorBootstrap;
+using Microsoft.JSInterop;
 
 namespace Blazor.Pages
 {
+    
     public partial class Rooms
     {
         [Inject]
+        protected PreloadService PreloadService { get; set; } = default!;
+        [Inject]
         public APIService _aPIService { get; set; } = default!;
-        
+
         public IEnumerable<RoomTypeDto> roomTypes { get; set; } = new List<RoomTypeDto>();
 
         protected override async Task OnInitializedAsync()
         {
+            PreloadService.Show();
+
             System.Console.WriteLine("OnInitializedAsync: Getting room types");
             var result = await _aPIService.GetRoomTypeAsync()
                 ?? throw new ArgumentException("Error: No Room types were found");
             roomTypes = result?.Where(r => r != null)!;
+
+            PreloadService.Hide();
         }
     }
 }
