61840b9c02d13fd2a530931c23897ae2d3147c75
Jasmin May Anderson <05jasmay@gmail.com>
2025-10-01 08:59:16 +0200

    Replace Preload with custom loading spinner in Rooms page

--- Blazor/Pages/Rooms.razor.cs
+++ Blazor/Pages/Rooms.razor.cs
@@ -1,11 +1,6 @@
-using Blazor.Models;
-using Blazor.Models.ViewModels;
-using DomainModels.Dto;
+
 using Microsoft.AspNetCore.Components;
-using DomainModels.Models;
 using Blazor.Services;
-using BlazorBootstrap;
-using Microsoft.JSInterop;
 
 namespace Blazor.Pages
 {
@@ -13,22 +8,21 @@ namespace Blazor.Pages
     public partial class Rooms
     {
         [Inject]
-        protected PreloadService PreloadService { get; set; } = default!;
-        [Inject]
         public APIService _aPIService { get; set; } = default!;
-
         public IEnumerable<RoomTypeDto> roomTypes { get; set; } = new List<RoomTypeDto>();
+        public bool IsLoading = false;
 
         protected override async Task OnInitializedAsync()
         {
-            PreloadService.Show();
+
+            IsLoading = true;
 
             System.Console.WriteLine("OnInitializedAsync: Getting room types");
             var result = await _aPIService.GetRoomTypeAsync()
                 ?? throw new ArgumentException("Error: No Room types were found");
             roomTypes = result?.Where(r => r != null)!;
 
-            PreloadService.Hide();
+            IsLoading = false;
         }
     }
 }
