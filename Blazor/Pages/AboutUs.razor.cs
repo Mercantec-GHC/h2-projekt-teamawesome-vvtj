8ad7ab046c33c8ad3dffb30ba1f3d203938cafd4
Jasmin May Anderson <05jasmay@gmail.com>
2025-10-03 11:15:04 +0200

    Add hotel listing to About Us page with loading spinner

--- Blazor/Pages/AboutUs.razor.cs
+++ Blazor/Pages/AboutUs.razor.cs
@@ -4,5 +4,17 @@ using Microsoft.AspNetCore.Components;
 
 namespace Blazor.Pages
 {
+    public partial class AboutUs
+    {
+        public List<HotelDto> Hotels { get; set; } = new();
+        public bool IsLoading = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsLoading = true;
+            Hotels = (await ApiService.GetAllHotelsAsync())?.ToList() ?? new List<HotelDto>();
+            IsLoading = false;
+        }
+    }
     
 }
